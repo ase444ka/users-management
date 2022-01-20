401ba7e0fe464bbbdf2199915c98944143c68a2a
Ase444ka <istomina.asia@yandex.ru>
2022-01-20 11:35:30 +0300

    App.vue: add layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Users logo" src="./assets/logo.png" />
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <v-container class="py-0 fill-height">
+        <v-avatar color="primary" size="36">
+          <img alt="Users logo" src="./assets/logo.png" />
+        </v-avatar>
+        <v-toolbar-title class="ml-15"
+          >Система управления пользователями</v-toolbar-title
+        >
+        <v-spacer></v-spacer>
+        <v-btn class="mr-15">Добавить пользователя</v-btn>
+        <v-btn>Выйти</v-btn>
+      </v-container>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
